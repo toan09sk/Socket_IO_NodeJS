6afa66885d4180d3a0a1c7af98469a3412055f0f
Ross <phambatoanbk@gmail.com>
2019-10-10 14:41:08 +0700

    Socket IO
    lang nghe phia server
    goi dien tu client

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ app.set("view engine", "ejs");
 app.set("views", "./views");
 
 var server = require("http").Server(app);
+var io = require("socket.io")(server);
 server.listen(3000);
+
+io.on("connection", (socket) => {
+    console.log("co nguoi ket noi");
+})
 
 app.get("/", (req, res) => {
     res.render("trangchu");
